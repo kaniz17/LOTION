a4ee4d0ed89dfe5f2d64934dbbbdc538a480d66b
kaniz17 <kaniz.taiba@ucalgary.ca>
2023-03-10 00:33:40 -0500

    changes

---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./index.css";
+
+
+function Headbar() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const hideSideBar = () => {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("hide");
+  };
+  const handleDarkModeClick = () => {
+    setIsDarkMode(!isDarkMode);
+    const root = document.documentElement;
+    root.classList.toggle("dark-mode");
+  };
+
+
+  return (
+    <div className="Headbar">
+      <div id="title_bar">
+        <button id="hide_sidebar" onClick={hideSideBar}>
+          &#9776;{" "}
+        </button>
+
+
+        <div id="center_text">
+          <h1> Lotion </h1>
+          <p id="subheading">
+            {" "}
+            Notion, but worse.{" "}
+          </p>
+        </div>
+
+
+        
+      </div>
+    </div>
+  );
+}
+
+
+export default Headbar;
